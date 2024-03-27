d6ee76326e0ac705e0362a681d7a980ccc44bdd3
DINESH2912 <99465507+DINESH2912@users.noreply.github.com>
2024-03-27 17:54:46 +0530

    Time: 1823 ms (62.44%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT product_id, product_name
+FROM Product p
+WHERE EXISTS (
+    SELECT product_id
+    FROM Sales
+    WHERE product_id = p.product_id AND sale_date BETWEEN '2019-01-01' AND '2019-03-31'
+)
+AND NOT EXISTS (
+    SELECT product_id
+    FROM Sales
+    WHERE product_id = p.product_id AND (sale_date < '2019-01-01' OR sale_date > '2019-03-31')
+);
+
