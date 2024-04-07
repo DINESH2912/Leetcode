099523c34c75174cefa374342f937f87721ea3ba
DINESH2912 <99465507+DINESH2912@users.noreply.github.com>
2024-04-07 21:52:12 +0530

    Time: 1600 ms (29.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,13 @@
+SELECT
+    p.product_name,
+    SUM(o.unit) AS unit
+FROM
+    Orders o
+    JOIN Products p ON o.product_id = p.product_id
+WHERE
+    MONTH(o.order_date) = 2
+    AND YEAR(o.order_date) = 2020
+GROUP BY
+    p.product_name
+HAVING
+    SUM(o.unit) >= 100;
