68cd462104793255ea87780278550dc8dd9928e4
DINESH2912 <99465507+DINESH2912@users.noreply.github.com>
2024-04-08 19:09:30 +0530

    Time: 1032 ms (35.75%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT name FROM Customer WHERE referee_id is NULL or referee_id <> 2;
+SELECT name FROM Customer WHERE referee_id is NULL or referee_id != 2;
