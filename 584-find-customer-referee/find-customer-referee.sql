0db9dcb41d772b737652aea262e928e9daec531b
DINESH2912 <99465507+DINESH2912@users.noreply.github.com>
2024-07-09 07:43:42 +0530

    Time: 391 ms (96.39%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,2 +1,7 @@
 # Write your MySQL query statement below
 select name from Customer where id not  in (select id from Customer where referee_id=2);
+
+#another approach is
+#select name from Customer where referee_id!=2 or referee_id is NULL        
+
+    
